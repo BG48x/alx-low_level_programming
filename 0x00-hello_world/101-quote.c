3db7052e67791343eb43387279dbe3e1e205cb75
BG48x <binu86226@gmail.com>
2023-09-07 12:39:29 -0700

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,17 @@
 #include <unistd.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 1 (Sucess)
+ * main - prints "and that piece of art is useful" -Dora korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: Always 1.
  */
+
+
 int main(void)
+
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	return (1);
 }
 
 
