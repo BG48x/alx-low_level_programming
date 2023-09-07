9f8993a1a0a4a41a7d4accdda446260f154a4d04
BG48x <binu86226@gmail.com>
2023-09-07 11:47:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,16 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * Return 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
 int main(void)
 
+
 {
 	puts("\"programming is like building a multilingual puzzle");
+
 	return (0);
 }
 
