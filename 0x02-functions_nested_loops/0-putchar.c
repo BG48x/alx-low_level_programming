573d8862d900a9bcbbcbb0bbf4ab084dbcdaebd8
BG48x <binu86226@gmail.com>
2023-09-13 11:25:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print - _putchar 
+ *
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char * = "Binyam"\n"
+
+	while (*h)
+        {
+	_putchar(*h);
+	++;
+        }
+	return (0);
+
+
+
+}
