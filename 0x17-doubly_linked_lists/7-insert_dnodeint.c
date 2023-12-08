8ad524312088b229f0294b5e09ed5df2f2d1c875
BG48x <binu86226@gmail.com>
2023-12-07 18:49:22 -0800

    8-delete_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ * @h: A pointer to the head of the distint_t list
+ * @idx: the postion to insert the new mode
+ * @n: The integer for the new node to contains
+ *
+ * Return: if the function fails -NULL.
+ * 	otherwise - the address of the new node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlisaint
