266bf7daa3c0d0af9907bdf05567cd0ae629d8ae
BG48x <binu86226@gmail.com>
2023-12-11 00:38:54 -0800

    libdynamic.so

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: Destination valur
+ * @src: source value
+ * Return: thr pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
+
