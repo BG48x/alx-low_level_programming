16566a2613089d117d4ea842907741a36884bb11
BG48x <binu86226@gmail.com>
2023-12-23 03:28:19 -0800

    1-djb2.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrives a value associated with a key
+ *
+ * @ht: hash table you want to look into
+ * @key: the key you are looking for
+ *
+ * Return: the value associated with the element else NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *nodd;
+	unsigned long int index;
+
+	if (key == NULL || ht == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	nodo = ht->array[index];
+
+	for (; nodo != NULL; nodo = nodo->next)
+	{
+		if (strcmp(nodo->key, key) == 0)
+			return (node->value);
+	}
+	return (NULL);
+}
